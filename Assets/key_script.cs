730214ee7289964e879e163b0e50814a136c6c29
zawarudo21 <advait.parmar@gmail.com>
2024-11-14 01:02:38 -0500

    added snapping

---
+++ Assets/key_script.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class key_script : MonoBehaviour
+{
+
+    //[Serializable]
+    public int key_id;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void set_key_id(int key_id)
+    {
+        this.key_id = key_id;
+        gameObject.SetActive(false);
+    }
+}
