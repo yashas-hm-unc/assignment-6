a3f4224e5f3c3ceefda06fd3abfa899b0a94b657
Paul Parker <pparker@unc.edu>
2024-11-15 11:56:09 -0500

    Code puzzle and necessary scripts added - code puzzle in Assets/prefabs, necessary scripts in Assets/scripts

---
+++ Assets/scripts/DigitNumberScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class DigitNumberScript : MonoBehaviour
+{
+    public TextMeshProUGUI codeTextValue;
+
+    public void addDigit(string digit)
+    {
+        if (digit.Equals("Good job!"))
+        {
+            codeTextValue.text = "Good job!";
+        }
+
+        if (!codeTextValue.text.Equals("Good job!"))
+        {
+            if (digit.Equals("X"))
+            {
+                if (codeTextValue.text == "Enter Code" || codeTextValue.text == "" || codeTextValue.text == "Wrong")
+                {
+                    codeTextValue.text = "";
+                }
+                else
+                {
+                    codeTextValue.text = codeTextValue.text.Substring(0, (codeTextValue.text.Length - 1));
+                }
+            }
+            else if (digit.Equals("C"))
+            {
+                codeTextValue.text = "Enter Code";
+            }
+            else
+            {
+                if (codeTextValue.text == "Enter Code" || codeTextValue.text == "Wrong")
+                {
+                    codeTextValue.text = "";
+                }
+                codeTextValue.text += digit;
+            }
+        }
+       
+    }
+    public string getDigit()
+    {
+        return codeTextValue.text;
+    }
+
+    public void wrongAnswer()
+    {
+        codeTextValue.text = "Wrong";
+    }
+}
---
+++ Assets/scripts/KeyPadScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class KeyPadScript : MonoBehaviour
+{
+    public DigitNumberScript digitNumber;
+    public GameObject KeyPad;
+
+    public void PressEnter()
+    {
+        if (digitNumber.getDigit() == "042")
+        {
+            digitNumber.addDigit("C");
+            digitNumber.addDigit("Good job!");
+            // update state of overall game, as this puzzle has been solved
+        }
+        else if (!(digitNumber.getDigit() == "Good job!"))
+        {
+            digitNumber.wrongAnswer();
+        }
+    }
+}
