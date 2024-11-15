8d56efc067b8a55cff2c26300ca67d0e191ea720
Paul Parker <pparker@unc.edu>
2024-11-15 13:25:12 -0500

    Fixed codepuzzle

---
+++ Assets/scripts/ButtonContact.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonContact : MonoBehaviour
+{
+    public Button button; // Assign the button in the Inspector
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+            button.onClick.Invoke(); // Trigger the button's OnClick() event
+    }
+}
